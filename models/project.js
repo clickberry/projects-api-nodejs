9f52f3561cf0aed182d6a2f8f067b569e2b2245b
altuhov <altuhov@clickberry.com>
2015-09-28 18:47:31 +0300

    Added BL.

--- models/project.js
+++ models/project.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 
 var projectSchema = mongoose.Schema({
     userId: String,
@@ -7,9 +8,105 @@ var projectSchema = mongoose.Schema({
     metadataUri: String,
     created: Date,
     isPrivate: Boolean,
-    videos: [],
-    screenshots: [],
-    images: []
+    videos: [new Schema({
+        contentType: String,
+        uri: String,
+        width: Number,
+        height: Number
+    }, {_id: false})],
+    screenshots: [new Schema({
+        contentType: String,
+        uri: String
+    }, {_id: false})],
+    images: [new Schema({
+        contentType: String,
+        uri: String
+    }, {_id: false})]
 });
 
-module.exports = mongoose.model('Project', projectSchema);
+var Project = module.exports = mongoose.model('Project', projectSchema);
+
+projectSchema.statics.create = function (userId, data, callback) {
+    var project = new Project({
+        userId: userId,
+        name: data.name,
+        description: data.description,
+        metadataUri: data.metadataUri,
+        videos: data.videos,
+        screenshots: data.screenshots,
+        images: data.images,
+        created: moment.utc()
+    });
+
+    project.save(function (err) {
+        callback(err, project);
+    });
+};
+
+projectSchema.statics.findNext = function (lastProjectId, top, callback) {
+    Project.find(
+        {
+            isPrivate: false,
+            _id: {$lt: lastProjectId}
+        }, null, {
+            sort: {created: -1},
+            limit: top
+        }, function (err, projects) {
+            callback(err, projects);
+        }
+    );
+};
+
+projectSchema.statics.getById = function (projectId, userId, callback) {
+    Project.findById(projectId, function (err, project) {
+        if (err) {
+            return callback(err);
+        }
+
+        if (!project) {
+            return callback(new Error('Not found project.'));
+        }
+
+        if (project.userId != userId && project.isPrivate) {
+            return callback(new Error('Forbidden to get project.'));
+        }
+
+        callback(null, project);
+    });
+};
+
+projectSchema.statics.edit = function (userId, projectId, editedFields, callback) {
+    Project.getById(projectId, userId, function (err, project) {
+        if (err) {
+            return callback(err);
+        }
+
+        if (project.userId != userId) {
+            return callback(new Error('Forbidden to edit project.'));
+        }
+
+        project.update(editedFields, function (err, newProject) {
+            if (err) {
+                return callback(err);
+            }
+
+            callback(null, newProject);
+        });
+    });
+};
+
+projectSchema.statics.delete = function (projectId, userId, callback) {
+    Project.getById(projectId, userId, function (err, project) {
+        if (err) {
+            return callback(err);
+        }
+
+        if (project.userId != userId) {
+            return callback(new Error('Forbidden to delete project.'));
+        }
+
+        project.remove(function (err) {
+            callback(err);
+        });
+    });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 
+var Project = require('../models/project');
+
 var Bus = require('../lib/bus-service');
 var bus = new Bus({});
 
@@ -12,38 +14,133 @@ module.exports = function (passport) {
 
     router.post('/',
         passport.authenticate('access-token', {session: false, assignProperty: 'payload'}),
-        function(req,res, next){
+        //signature.checkVideo,
+        function (req, res, next) {
+            var userId = req.payload.userId;
 
-    });
+            Project.create(userId, req.body, function (err, project) {
+                if (err) {
+                    return next(err);
+                }
+
+                var projectDto = projectMapper(project);
+                res.status(201);
+                res.send(projectDto);
+            });
+        });
 
     router.get('/',
         passport.authenticate('access-token', {session: false, assignProperty: 'payload'}),
-        function(req,res, next){
+        function (req, res, next) {
+            var userId = req.payload.userId;
+            Project.find({userId: userId}, function (err, projects) {
+                if (err) {
+                    return next(err);
+                }
 
-    });
+                var projectDtos = projects.map(projectMapper);
+                res.send(projectDtos);
+            });
+        });
 
     router.get('/all',
-        function(req,res, next){
+        function (req, res, next) {
+            var lastId = req.query.last;
+            var top = req.query.top;
 
+            Project.findNext(lastId, top, function (err, projects) {
+                if (err) {
+                    return next(err);
+                }
+
+                var projectDtos = projects.map(projectMapper);
+                res.send(projectDtos);
+            });
         });
 
     router.get('/:projectId',
         passport.authenticate('access-token', {session: false, assignProperty: 'payload'}),
-        function(req,res, next){
+        function (req, res, next) {
+            var userId = req.payload.userId;
+            var projectId = req.params.projectId;
+            Project.getById(projectId, userId, function (err, project) {
+                if (err) {
+                    return next(err);
+                }
 
-    });
+                var projectDto = projectMapper(project);
+                res.send(projectDto);
+            });
+        });
 
     router.put('/:projectId',
         passport.authenticate('access-token', {session: false, assignProperty: 'payload'}),
-        function(req,res, next){
+        function (req, res, next) {
+            var userId = req.payload.userId;
+            var projectId = req.params.projectId;
+            var editedFields = mapToEditedFields(req.body);
 
+            Project.edit(projectId, userId, editedFields, function (err, project) {
+                if (err) {
+                    return next(err);
+                }
+
+                var projectDto = projectMapper(project);
+                res.send(projectDto);
+            });
         });
 
     router.delete('/:projectId',
         passport.authenticate('access-token', {session: false, assignProperty: 'payload'}),
-        function(req,res, next){
+        function (req, res, next) {
+            var userId = req.payload.userId;
+            var projectId = req.params.projectId;
+            Project.delete(projectId, userId, function (err) {
+                if (err) {
+                    return next(err);
+                }
 
-    });
+                res.sendStatus(200);
+            });
+        });
 
     return router;
 };
+
+function projectMapper(project) {
+    return {
+        id: project._id,
+        userId: project.userId,
+        name: project.name,
+        description: project.description,
+        metadataUri: project.metadataUri,
+        videos: project.videos,
+        screenshots: project.screenshots,
+        images: project.images,
+        created: project.created
+    };
+}
+
+function mapToEditedFields(project) {
+    var editedFields = {};
+    if (project.name) {
+        editedFields.name = project.name;
+    }
+    if (project.description) {
+        editedFields.description = project.description;
+    }
+    if (project.metadataUri) {
+        editedFields.metadataUri = project.metadataUri;
+    }
+    if (project.images) {
+        editedFields.images = project.images;
+    }
+
+    return project;
+    //return {
+    //    name: project.name || undefined,
+    //    description: project.description || undefined,
+    //    metadataUri: project.metadataUri || undefined,
+    //    images: project.images || undefined
+    //}
+}
